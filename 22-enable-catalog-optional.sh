23bbd788d94dc04a69db9f5a376cb7a717ad7a33
keyvan pishevar <kpisheva@redhat.com>
2021-04-08 11:37:10 -0500

    Delete 22-enable-catalog-optional.sh

--- 22-enable-catalog-optional.sh
+++
@@ -1,6 +0,0 @@
-cd ocp4
-export KUBECONFIG=./auth/kubeconfig
-oc edit servicecatalogapiservers
-oc edit servicecatalogcontrollermanagers
-echo "Login to UI create namespace openshift-template-service-broker and install service broker"
-echo "Select the installed operator and Under Provided APIs, click Create New for Template Service Broker"
