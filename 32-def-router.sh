28ef81427fcefaa8c86749d6963806b4e97e52bf
keyvan pishevar <kpisheva@redhat.com>
2021-04-08 11:37:37 -0500

    Delete 32-def-router.sh

--- 32-def-router.sh
+++
@@ -1,6 +0,0 @@
-cd ./ocp4
-export KUBECONFIG=./auth/kubeconfig
-# oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"storage":{"emptyDir":{}}}}'
-oc patch configs.imageregistry.operator.openshift.io/cluster --type merge -p '{"spec":{"defaultRoute":true}}'
-watch oc get csr
-
