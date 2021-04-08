01c54bcb363244846bfa1172ede504db6d39c003
keyvan pishevar <kpisheva@redhat.com>
2021-04-08 11:36:41 -0500

    Delete 12-OMV-postinstall-setup-nfs.sh

--- 12-OMV-postinstall-setup-nfs.sh
+++
@@ -1,5 +0,0 @@
-cd ocp4
-export KUBECONFIG=./auth/kubeconfig
-oc create -f ../templates/nfs-rbac.yaml
-oc create -f ../templates/nfs-sc.yaml
-oc create -f ../templates/OMV-nfs-provisioner-deployment.yaml
